019337b7ce2f3fa5342a2792b4067a8a502639d1
earGO <rh.edwin@gmail.com>
2022-09-02 17:40:36 -0400

    added commentaries about further actions

--- src/App.tsx
+++ src/App.tsx
@@ -19,15 +19,25 @@ import {useState} from "react";
 
 
 function App() {
+    /* current user ID was supposed to be taken from URL with useParams() hook of React Router, to pass it to useEffect as a parameter to get user's state */
 
-    const [currentSubmissionId, setCurrentSubmissionId] = useState<null|string>(EXAMPLE_SUBMISSIONS[0].id)
+    const [currentSubmissionId, setCurrentSubmissionId] = useState<null | string>(EXAMPLE_SUBMISSIONS[0].id)
+
+    /*
+    * next steps would've been having useEffect trying to fetch app state for a user from local store
+    * and then setting current submission ID to the next submission ID after the one already voted by user
+    * */
 
     const handleUpVote = (submissionId: string): void => {
         console.log(submissionId)
+        /* this was supposed to persist user's actions to the local store for the upVote, and then increase the submission index by 1 to show next one */
+
     }
 
     const handleDownVote = (submissionId: string): void => {
         console.log(submissionId)
+        /* this was supposed to persist user's actions to the local store for the downVote  and then increase the submission index by 1 to show next one */
+
     }
 
     return (
